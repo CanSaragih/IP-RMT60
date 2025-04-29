91eb8d7914d467c5bdb0209182aa74d6fc740e5b
CanSaragih <canwhardana@gmail.com>
2025-04-30 01:04:21 +0700

    chore: fix login by Google

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,7 @@
 const { signToken } = require("../helpers/jwt");
 const { User } = require("../models");
 const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client();
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 module.exports = class UserController {
   static async loginGoogle(req, res, next) {
@@ -12,12 +12,13 @@ module.exports = class UserController {
         idToken: googleToken,
         audience: process.env.GOOGLE_CLIENT_ID,
       });
+
       const payload = ticket.getPayload();
 
       let user = await User.findOne({ where: { email: payload.email } });
 
       if (!user) {
-        await User.create({
+        user = await User.create({
           email: payload.email,
           username: payload.name,
           avatarUrl: payload.picture,
@@ -25,6 +26,7 @@ module.exports = class UserController {
           providerId: payload.sub,
         });
       }
+
       const access_token = signToken({ id: user.id });
       res.json({ access_token });
     } catch (error) {
