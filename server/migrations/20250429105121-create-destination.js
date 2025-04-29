ab011aafd235c87e69a5be773750170228281958
CanSaragih <canwhardana@gmail.com>
2025-04-29 19:02:04 +0700

    chore: seed all tables with initial dummy data

--- server/migrations/20250429105121-create-destination.js
+++ server/migrations/20250429105121-create-destination.js
@@ -1,37 +1,37 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Destinations', {
+    await queryInterface.createTable("Destinations", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       googlePlaceId: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       latitude: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       longitude: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Destinations');
-  }
+    await queryInterface.dropTable("Destinations");
+  },
 };
---
+++ server/seeders/20250429111718-seed-data.js
@@ -0,0 +1,75 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const trips = require("../data/trip.json");
+    const itineraries = require("../data/itineraries.json");
+    const budgetItems = require("../data/budgetItems.json");
+    const destinations = require("../data/destination.json");
+    const detailDestinations = require("../data/destinationDetail.json");
+
+    await queryInterface.bulkInsert(
+      "Trips",
+      trips.map((trip) => {
+        return {
+          ...trip,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
+
+    await queryInterface.bulkInsert(
+      "Itineraries",
+      itineraries.map((itinerary) => {
+        return {
+          ...itinerary,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
+
+    await queryInterface.bulkInsert(
+      "BudgetItems",
+      budgetItems.map((budgetItem) => {
+        return {
+          ...budgetItem,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
+
+    await queryInterface.bulkInsert(
+      "Destinations",
+      destinations.map((destination) => {
+        return {
+          ...destination,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
+
+    await queryInterface.bulkInsert(
+      "DestinationDetails",
+      detailDestinations.map((detailDestination) => {
+        return {
+          ...detailDestination,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        };
+      })
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("DetailDestinations", null, {});
+    await queryInterface.bulkDelete("Destinations", null, {});
+    await queryInterface.bulkDelete("BudgetItems", null, {});
+    await queryInterface.bulkDelete("Itineraries", null, {});
+    await queryInterface.bulkDelete("Trips", null, {});
+  },
+};
