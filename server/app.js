4ad5b70e619e561bdf5a4101d85bd540de3c73b1
CanSaragih <canwhardana@gmail.com>
2025-07-28 15:21:18 +0700

    add cors

--- server/app.js
+++ server/app.js
@@ -10,7 +10,12 @@ const router = require("./routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: "true",
+  })
+);
 
 app.use(router);
 
